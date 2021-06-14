bb90eac5fcb161d4ea5fd5ea98957fdb8a753d41
RozanovaSvetlana <khitrik.rozanova.svetlana@gmail.com>
2021-06-14 13:19:43 +0300

    feat(back): добавлены необходимые переменные

--- app/src/main/java/com/example/a2048/MainActivity.java
+++ app/src/main/java/com/example/a2048/MainActivity.java
@@ -3,8 +3,20 @@ package com.example.a2048;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String MAIN_ACTIVITY_TAG = "2048_MainActivity";
+
+    private WebView mWebView;
+    private long mLastBackPress;
+    private static final long mBackPressThreshold = 3500;
+    private static final String IS_FULLSCREEN_PREF = "is_fullscreen_pref";
+    private long mLastTouch;
+    private static final long mTouchThreshold = 2000;
+    private Toast pressBackToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
